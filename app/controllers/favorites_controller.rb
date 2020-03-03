6a9628068057513ae809641f950d684838fbfcdb
Riu <y1a1r2y5u@gmail.com>
2020-03-03 17:20:37 +0900

    [Start-3]

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,20 @@
+class FavoritesController < ApplicationController
+	def create
+		book = Book.find(params[:book_id])
+		favorite = current_user.favorites.new(book_id: book.id)
+		# 上の記述と同じ効果
+		# favorite = Favorite.new(user_id: current_user.id, book_id: @book.id)
+		favorite.save
+		redirect_to books_path
+		# -favoritesの流れ-
+		# favorite controllerでcreateのactionが呼び出されるとBook.findで本を探し出して
+		#  favorite.newでいいねを作成。その際のuser.id はcurrentで指定、book_idは（book_id: @book.id）で指定して
+		#  favorite.saveでshemaのfavoritesに格納される
+	end
+	def destroy
+		book = Book.find(params[:book_id])
+		favorite = current_user.favorites.find_by(book_id: book.id)
+		favorite.destroy
+		redirect_to books_path
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   # get "users/:id" => "users#show"
-  resources :books
+  resources :books do
+  	resource :favorites, only: [:create, :destroy]
+  end
   root 'home#top'
   get 'home/about' => 'home#about'
 
